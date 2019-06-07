7175405d2af8a98c7eeadc9efc6bfbd5edfce35d
blackmoia <51493636+blackmoia@users.noreply.github.com>
2019-06-07 17:11:48 -0300

    Update Veiculo.java

--- Veiculo.java
+++ Veiculo.java
@@ -1,12 +1,16 @@
 
 package locadora.de.veiculos;
-public class Veiculo {
-    String placa;
-    String cor;
-    String modelo;
-    String fabricante;
-    String anoFabricacao;
-    float valorDiaria;
-    String situacao;
+public abstract class Veiculo {
+protected String placa;
+ proected String cor;
+ protected String modelo;
+ protected   String fabricante;
+  protected  String anoFabricacao;
+  protected  float valorDiaria;
+ protected   String situacao;
+    //metudos//
+    public abstract void lidar();
+    
+    public abstract void desligar();
     
 }
